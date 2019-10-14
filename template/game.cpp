b95c59e59f5587f305a203a1701427a3c3b177a5
= <=>
2019-10-14 19:08:38 +0200

    THIS IS INITIAL, NOTHING TO WORRY ABOUT.

---
+++ template/game.cpp
@@ -0,0 +1,17 @@
+#include "precomp.h" // include (only) this in every .cpp file
+
+// -----------------------------------------------------------
+// Initialize the application
+// -----------------------------------------------------------
+void Game::Init()
+{
+
+}
+
+// -----------------------------------------------------------
+// Main application tick function
+// -----------------------------------------------------------
+void Game::Tick( float deltaTime )
+{
+
+}
